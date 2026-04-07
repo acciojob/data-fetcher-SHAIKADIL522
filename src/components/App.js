5c59856642a89e202c2bbd3b391059f1bbf3a5a8
SHAIKADIL522 <your-github-email@gmail.com>
2026-04-07 14:00:19 +0530

    Fixed regeneratorRuntime issue

--- src/components/App.js
+++ src/components/App.js
@@ -2,43 +2,45 @@ import React from "react";
 import "./../styles/App.css";
 
 class App extends React.Component {
-  state = {
-    data: null,
-    error: null,
-    loading: true,
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      error: null,
+      loading: true,
+    };
+  }
 
-  async componentDidMount() {
-    try {
-      const response = await fetch("https://dummyjson.com/products");
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch");
-      }
-
-      const json = await response.json();
-
-      // Handle empty data properly
-      if (!json || Object.keys(json).length === 0) {
+  componentDidMount() {
+    fetch("https://dummyjson.com/products")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        if (!json || Object.keys(json).length === 0) {
+          this.setState({
+            data: [],
+            error: null,
+            loading: false,
+          });
+        } else {
+          this.setState({
+            data: json,
+            error: null,
+            loading: false,
+          });
+        }
+      })
+      .catch((err) => {
         this.setState({
-          data: [],
-          error: null,
+          error: err.message,
+          data: null,
           loading: false,
         });
-      } else {
-        this.setState({
-          data: json,
-          error: null,
-          loading: false,
-        });
-      }
-    } catch (err) {
-      this.setState({
-        error: err.message,
-        data: null,
-        loading: false,
       });
-    }
   }
 
   render() {
@@ -53,7 +55,7 @@ class App extends React.Component {
         ) : loading ? (
           <pre>Loading...</pre>
         ) : data && Array.isArray(data) && data.length === 0 ? (
-          <pre>{JSON.stringify(data)}</pre> // must be []
+          <pre>{JSON.stringify(data)}</pre> // []
         ) : (
           <>
             <h3>Data Fetched from API</h3>
