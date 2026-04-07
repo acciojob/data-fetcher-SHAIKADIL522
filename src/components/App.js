b0ab5225be50fa894e4f690add3a950a05102494
SHAIKADIL522 <your-github-email@gmail.com>
2026-04-07 13:43:07 +0530

    fix: use Promise chain instead of async/await to fix regeneratorRuntime error

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,52 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./../styles/App.css";
 
-const App = () => {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await fetch("https://dummyjson.com/products");
-        const json = await response.json();
-        setData(json);
-      } catch (error) {
-        console.error("Failed to fetch products:", error);
-      }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      error: null,
     };
+  }
 
-    fetchProducts();
-  }, []);
+  componentDidMount() {
+    fetch("https://dummyjson.com/products")
+      .then(function (response) {
+        if (!response.ok) {
+          throw new Error("Failed to fetch");
+        }
+        return response.json();
+      })
+      .then(function (json) {
+        if (!json || Object.keys(json).length === 0) {
+          this.setState({ data: "No data found", error: null });
+        } else {
+          this.setState({ data: json, error: null });
+        }
+      }.bind(this))
+      .catch(function (err) {
+        this.setState({ error: err.message, data: null });
+      }.bind(this));
+  }
 
-  return (
-    <div>
-      {/* Do not remove the main div */}
-      <pre>{data ? JSON.stringify(data, null, 2) : "Loading..."}</pre>
-    </div>
-  );
-};
+  render() {
+    var content;
+    if (this.state.error) {
+      content = this.state.error;
+    } else if (!this.state.data) {
+      content = "No data found";
+    } else {
+      content = JSON.stringify(this.state.data, null, 2);
+    }
+
+    return (
+      <div>
+        {/* Do not remove the main div */}
+        <pre>{content}</pre>
+      </div>
+    );
+  }
+}
 
 export default App;
